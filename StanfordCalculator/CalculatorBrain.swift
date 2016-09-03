eb232efa961f43053661ffe8cbb254f2a14dd6ac
Fabio Cipriani <FabioCipriani@mbp-di-fabio.station>
2016-09-03 12:44:36 +0200

    UI Setup added CalculatorBrain.swift file to contain CalculatorBrain class for our Model part of the app

---
+++ StanfordCalculator/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  StanfordCalculator
+//
+//  Created by Fabio Cipriani on 03/09/16.
+//  Copyright © 2016 Fabio Cipriani. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    
+}
--- StanfordCalculator/ViewController.swift
+++ StanfordCalculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var btn: UIButton!
+    @IBOutlet private weak var resultLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnPressed(_ sender: AnyObject) {
+    }
+    
+    
 }
 
